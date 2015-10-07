16ee7567989f04f7e8685ff2432f26a3fd48eb76
Harish V <harishv207@gmail.com>
2015-10-07 19:20:42 +0800

    Added WelcomeScene Code
    Added the relevant code, but Welcome Scene is not displayed yet

--- src/fini/main/MainApp.java
+++ src/fini/main/MainApp.java
@@ -6,18 +6,31 @@ import java.util.prefs.Preferences;
 
 import fini.main.model.Task;
 import fini.main.view.TaskOverviewController;
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class MainApp extends Application {
- private Stage primaryStage;
+ protected static final double OPACITY_ZERO = 0;
+protected static final double OPACITY_FULL = 100;
+protected static final double DURATION_OF_FADE_TRANSITION = 3000;
+private Stage primaryStage;
+ private Scene welcomeScreen;
  private BorderPane rootLayout;
+ private AnchorPane welcomeLayout;
  
  private ObservableList<Task> taskData = FXCollections.observableArrayList();
  
@@ -35,14 +48,61 @@ public class MainApp extends Application {
  }
  
  @Override
- public void start(Stage primaryStage) {
+ public void start(Stage primaryStage) throws IOException {
   this.primaryStage = primaryStage;
   this.primaryStage.setTitle("Fini");
-  
+  displayWelcomeScene();
+  fadeOutWelcomeScene();
+  //fadeInMainScene();
   initRootLayout();
   
   showTaskOverview();
  }
+
+ private void displayWelcomeScene() throws IOException {
+     FXMLLoader loader = new FXMLLoader();
+     loader.setLocation(MainApp.class.getResource("view/WelcomeScene.fxml"));
+     welcomeLayout = (AnchorPane) loader.load();
+     welcomeScreen = new Scene(welcomeLayout);
+     primaryStage.setScene(welcomeScreen);
+     primaryStage.show();
+     System.out.println("SHowing WELCOME");
+     SequentialTransition seqTransition = new SequentialTransition (
+         new PauseTransition(Duration.millis(30000)) // wait a second
+     );
+     seqTransition.play();
+ }
+
+ private void fadeOutWelcomeScene() {
+   FadeTransition welcomeSceneFadeOut = new FadeTransition(Duration.millis(DURATION_OF_FADE_TRANSITION), rootLayout);
+   welcomeSceneFadeOut.setFromValue(OPACITY_FULL);
+   welcomeSceneFadeOut.setToValue(OPACITY_ZERO);
+   welcomeSceneFadeOut.play();
+}
+ 
+ private void fadeInMainScene() {
+   Timeline timeline = new Timeline();
+   timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(0.5),
+       new EventHandler<ActionEvent>() {
+
+           @Override
+           public void handle(ActionEvent event) {
+               try {
+                   rootLayout = FXMLLoader.load(getClass().getResource("view/RootLayout.fxml"));
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+               FadeTransition mainSceneFadeIn = new FadeTransition(Duration.millis(DURATION_OF_FADE_TRANSITION), rootLayout);
+               mainSceneFadeIn.setFromValue(OPACITY_ZERO);
+               mainSceneFadeIn.setToValue(OPACITY_FULL);
+               mainSceneFadeIn.play();
+               primaryStage.setScene(new Scene(rootLayout));
+               primaryStage.show();
+           }
+       }));
+   timeline.play();
+}
+
  
  public void initRootLayout() {
   try {
@@ -74,6 +134,7 @@ public class MainApp extends Application {
    // Give the controller access to the main app.
    TaskOverviewController controller = loader.getController();
    controller.setMainApp(this);
+   System.out.println("Controller Set!");
   } catch (IOException e) {
    e.printStackTrace();
   }
