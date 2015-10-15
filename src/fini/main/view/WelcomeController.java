7beefdfff09ba20aa6bfaa47f1012d7926bc76c2
Harish V <harishv207@gmail.com>
2015-10-16 00:50:19 +0800

    Added Welcome Scene and some CSS

---
+++ src/fini/main/view/WelcomeController.java
@@ -0,0 +1,18 @@
+package fini.main.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class WelcomeController {
+	@FXML
+	private Button welcomeButton;
+	
+	@FXML
+	public void handleKeyPress(KeyEvent event) {
+		if(event.getCode() == KeyCode.ENTER) {
+			
+		}
+	}
+}
