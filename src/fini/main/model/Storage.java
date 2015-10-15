bd1b412cac16274ac0c1806f87732f07ff40b69c
Gai <gaieepo@gmail.com>
2015-10-15 16:54:04 +0800

    storage1

--- src/fini/main/model/Storage.java
+++ src/fini/main/model/Storage.java
@@ -1,18 +1,34 @@
 package fini.main.model;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+import com.google.gson.Gson;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.media.VideoTrack;
 
 public class Storage {
   private static Storage taskOrganiser;
   private ObservableList<Task> taskMasterList;
   
-  private File file;
+  private File saveFile;
+  private BufferedReader reader;
+  private PrintWriter writer;
+  private Gson gson;
   
   public Storage() {
     taskMasterList = FXCollections.observableArrayList();
+    gson = new Gson();
+    saveFile = new File("save.txt");
+    createIfNotExists(saveFile);
   }
   
   public static Storage getInstance() {
@@ -21,6 +37,16 @@ public class Storage {
     }
     return taskOrganiser;
   }
+  
+  private void createIfNotExists(File saveFile) {
+	  try {
+		  if (!saveFile.exists()) {
+			saveFile.createNewFile();
+		  }
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
+  }
 
   public void addNewTask(Task newTask) {
      taskMasterList.add(newTask);
@@ -28,5 +54,29 @@ public class Storage {
 
   public ObservableList<Task> getTasks() {
     return taskMasterList;
+  }
+  
+  public void readFile() {
+	  ArrayList<Task> storedTask;
+	  
+	  String text = "";
+	  
+	  // Init BufferedReader
+	  try {
+		  reader = new BufferedReader(new FileReader(saveFile));
+		  
+		  Task task = gson.fromJson(text, Task.class);
+		  storedTask.add(task);
+	  } catch (FileNotFoundException e) {
+		  storedTask = new ArrayList<Task>();
+	  }
+	  
+	  
+	  
+	  try {
+		  reader.close();
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
   }
 }
