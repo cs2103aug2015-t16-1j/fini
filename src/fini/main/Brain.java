bbab9d9d375401d0b623682292a217a2f75ab064
Gai <gaieepo@gmail.com>
2015-10-16 10:32:25 +0800

    brain1

--- src/fini/main/Brain.java
+++ src/fini/main/Brain.java
@@ -2,9 +2,11 @@ package fini.main;
 
 import fini.main.model.FiniParser;
 import fini.main.model.Storage;
+import fini.main.view.RootController;
 
 public class Brain {
 	private static Brain brain;
+	private RootController rootController;
 	
 	private Storage taskOrganiser;
 	private FiniParser parser;
@@ -14,7 +16,18 @@ public class Brain {
 		taskOrganiser = Storage.getInstance();
 	}
 	
+	public static Brain getInstance() {
+		if (brain == null) {
+			brain = new Brain();
+		}
+		return brain;
+	}
+	
 	public void executeCommand(String command) {
 		
+	}
+	
+	public void setRootController(RootController rootController) {
+		this.rootController = rootController;
 	}
 }
