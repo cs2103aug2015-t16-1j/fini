53dcf4cd2f40dbb956b45d6692c0d8e7caaa3f31
qiyun28 <yuqiyun28@gmail.com>
2015-10-18 10:25:21 +0800

    formatting and indentation

--- src/fini/main/MainApp.java
+++ src/fini/main/MainApp.java
@@ -18,72 +18,73 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /*
- * This is the main class for FINI (the application). The two scenes,
- * welcome scene and the main scene are handled here.
+ * This is the main class for FINI (the application). The two scenes, welcome scene and the main
+ * scene are handled here.
+ * 
  * @author A0121828H
  */
 
 public class MainApp extends Application {
-	// Global Logger
-	public final static Logger finiLogger = Logger.getLogger(MainApp.class.getName());
+    // Global Logger
+    public final static Logger finiLogger = Logger.getLogger(MainApp.class.getName());
 
-	@FXML
-	private Button welcomeButton;
+    @FXML
+    private Button welcomeButton;
 
-	private RootController rootController;
-	private Brain brain;
-	private Stage primaryStage = new Stage();
+    private RootController rootController;
+    private Brain brain;
+    private Stage primaryStage = new Stage();
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage stage) {
-		AnchorPane parent = null;
-		primaryStage = stage;
-		welcomeButton = new Button();
-		try {
-			parent = FXMLLoader.load(getClass().getResource("view/Welcome.fxml"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		Scene scene = new Scene(parent);
+    @Override
+    public void start(Stage stage) {
+        AnchorPane parent = null;
+        primaryStage = stage;
+        welcomeButton = new Button();
+        try {
+            parent = FXMLLoader.load(getClass().getResource("view/Welcome.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(parent);
 
-		setListenerForWelcomeScene(parent);
-		primaryStage.setTitle("Fini");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+        setListenerForWelcomeScene(parent);
+        primaryStage.setTitle("Fini");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-	private void setListenerForWelcomeScene(AnchorPane parent) {
-		final TextField welcomeSceneListener = new TextField();
-		welcomeSceneListener.setLayoutX(-200);
-		welcomeSceneListener.setLayoutY(-200);
-		welcomeSceneListener.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			public void handle(KeyEvent userPressesEnter) {
-				if(userPressesEnter.getCode().equals(KeyCode.ENTER)) {
-					Parent main = null;
-					try {
-						main = FXMLLoader.load(getClass().getResource("view/FiniLayout.fxml"));
-					} catch (IOException e) {
-						System.out.println("Unable to find or load FXML file");
-						e.printStackTrace();
-					}
-					Scene scene = new Scene(main);
-					primaryStage.setScene(scene);
-					primaryStage.show();
-					intialiseRootController();
-				}
-			}
-		});
-		parent.getChildren().add(welcomeSceneListener);
-		welcomeSceneListener.requestFocus();
-	}
+    private void setListenerForWelcomeScene(AnchorPane parent) {
+        final TextField welcomeSceneListener = new TextField();
+        welcomeSceneListener.setLayoutX(-200);
+        welcomeSceneListener.setLayoutY(-200);
+        welcomeSceneListener.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent userPressesEnter) {
+                if (userPressesEnter.getCode().equals(KeyCode.ENTER)) {
+                    Parent main = null;
+                    try {
+                        main = FXMLLoader.load(getClass().getResource("view/FiniLayout.fxml"));
+                    } catch (IOException e) {
+                        System.out.println("Unable to find or load FXML file");
+                        e.printStackTrace();
+                    }
+                    Scene scene = new Scene(main);
+                    primaryStage.setScene(scene);
+                    primaryStage.show();
+                    intialiseRootController();
+                }
+            }
+        });
+        parent.getChildren().add(welcomeSceneListener);
+        welcomeSceneListener.requestFocus();
+    }
 
-	private void intialiseRootController() {
-		rootController = new RootController();
-		brain = Brain.getInstance();
-		brain.setRootController(this.rootController);
-	}
+    private void intialiseRootController() {
+        rootController = new RootController();
+        brain = Brain.getInstance();
+        brain.setRootController(this.rootController);
+    }
 }
--- src/fini/main/view/TaskCategory.java
+++ src/fini/main/view/TaskCategory.java
@@ -13,23 +13,23 @@ public class TaskCategory extends HBox {
     private Label date;
 
     public TaskCategory(String taskCategory) throws Exception {
-	initialiseFxmlFiles();
-	category = new Label();
-	this.category.setText("HARISH");
+        initialiseFxmlFiles();
+        category = new Label();
+        this.category.setText("HARISH");
     }
 
     public TaskCategory(String taskCategory, String date) throws Exception {
-	initialiseFxmlFiles();
-	this.category = new Label();
-	this.date = new Label();
-	this.category.setText("HARISH");
-	this.date.setText(date);
+        initialiseFxmlFiles();
+        this.category = new Label();
+        this.date = new Label();
+        this.category.setText("HARISH");
+        this.date.setText(date);
     }
 
     private void initialiseFxmlFiles() throws Exception {
-	FXMLLoader loader = new FXMLLoader(getClass().getResource("TaskCategory.fxml"));
-	loader.setRoot(this);
-	loader.setController(this);
-	loader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("TaskCategory.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        loader.load();
     }
 }
