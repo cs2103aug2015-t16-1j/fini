2e98afda9d8d3bf28429f26087ed9fab25145141
Jonas <azeures@users.noreply.github.com>
2015-10-19 23:44:13 +0800

    Indentation - Command

--- src/fini/main/model/Command.java
+++ src/fini/main/model/Command.java
@@ -6,50 +6,50 @@ package fini.main.model;
  *
  */
 public class Command {
-    public static enum Type {
-        ADD, DELETE, INVALID
-    }
+	public static enum Type {
+		ADD, DELETE, INVALID
+	}
 
-    private String action;
-    private String arguments;
-    private Type type;
+	private String action;
+	private String arguments;
+	private Type type;
 
-    public Command(String command) {
-        action = getUserAction(command);
-        arguments = getUserArguments(command);
-        type = determineActionType(action);
-    }
+	public Command(String command) {
+		action = getUserAction(command);
+		arguments = getUserArguments(command);
+		type = determineActionType(action);
+	}
 
-    // Public Getters
-    public Type getCommandType() {
-        return type;
-    }
+	// Public Getters
+	public Type getCommandType() {
+		return type;
+	}
 
-    public String getCommandAction() {
-        return action;
-    }
+	public String getCommandAction() {
+		return action;
+	}
 
-    public String getCommandArguments() {
-        return arguments;
-    }
+	public String getCommandArguments() {
+		return arguments;
+	}
 
-    // Utility
-    private String getUserAction(String command) {
-        return command.trim().split("\\s+")[0];
-    }
+	// Utility
+	private String getUserAction(String command) {
+		return command.trim().split("\\s+")[0];
+	}
 
-    private String getUserArguments(String command) {
-        return command.replaceFirst(getUserAction(command), "").trim();
-    }
+	private String getUserArguments(String command) {
+		return command.replaceFirst(getUserAction(command), "").trim();
+	}
 
-    private Type determineActionType(String action) {
-        switch (action.toLowerCase()) {
-            case "add":
-                return Type.ADD;
-            case "delete":
-                return Type.DELETE;
-            default:
-                return Type.INVALID;
-        }
-    }
+	private Type determineActionType(String action) {
+		switch (action.toLowerCase()) {
+		case "add":
+			return Type.ADD;
+		case "delete":
+			return Type.DELETE;
+		default:
+			return Type.INVALID;
+		}
+	}
 }
