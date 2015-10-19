78632916811351b3e994cc4180402ec5b623b112
Jonas <azeures@users.noreply.github.com>
2015-10-19 23:43:55 +0800

    Indentation - Brain

--- src/fini/main/Brain.java
+++ src/fini/main/Brain.java
@@ -16,66 +16,66 @@ import javafx.collections.ObservableList;
  * @author gaieepo
  */
 public class Brain {
-    private static Brain brain;
-    private RootController rootController;
+	private static Brain brain;
+	private RootController rootController;
 
-    private Storage taskOrganiser;
-    private FiniParser parser;
-    private Sorter sorter;
+	private Storage taskOrganiser;
+	private FiniParser parser;
+	private Sorter sorter;
 
-    private ArrayList<Task> taskMasterList;
-    private ObservableList<Task> taskObservableList = FXCollections.observableArrayList();
-    
-    private Brain() {
-        parser = FiniParser.getInstance();
-        taskOrganiser = Storage.getInstance();
-        taskMasterList = taskOrganiser.readFile();
-        
-        sortAllTasks();
-        // 2. Load incomplete;
-    }
+	private ArrayList<Task> taskMasterList;
+	private ObservableList<Task> taskObservableList = FXCollections.observableArrayList();
 
-    public static Brain getInstance() {
-        if (brain == null) {
-            brain = new Brain();
-//            brain.initFirstDisplay();
-        }
-        return brain;
-    }
-    
-    private void initFirstDisplay() {
-//    	rootController.setFeedback("Welcome to fini");
-//    	rootController.updateMainDisplay(taskMasterList);
-    }
+	private Brain() {
+		parser = FiniParser.getInstance();
+		taskOrganiser = Storage.getInstance();
+		taskMasterList = taskOrganiser.readFile();
 
-    public void executeCommand(String command) {
-        boolean isOperationSuccessful;
-        isOperationSuccessful = parser.parse(command);
+		sortAllTasks();
+		// 2. Load incomplete;
+	}
 
-        // Change all of these hierarchy: TODO
-        rootController.updateMainDisplay(taskOrganiser.getTasks());
-        rootController.updateProjectsOverviewPanel(taskOrganiser.getTasks());
-        rootController.updateTasksOverviewPanel(taskOrganiser.getTasks());
-        rootController.updateDisplayToUser(isOperationSuccessful);
-        taskOrganiser.updateFile();
+	public static Brain getInstance() {
+		if (brain == null) {
+			brain = new Brain();
+			//            brain.initFirstDisplay();
+		}
+		return brain;
+	}
 
-        // Handle Using Command Class: TODO
-        // Command newCommand = new Command(command);
-        //
-        // Command.Type commandType = newCommand.getCommandType();
-        // String commandAction = newCommand.getCommandAction();
-        // String commandArguments = newCommand.getCommandArguments();
-    }
-    
-    // Init Methods
-    public void setRootController(RootController rootController) {
-        this.rootController = rootController;
-    }
-    
-    private void sortAllTasks() {
-    	assert taskMasterList != null;
-    	sorter = new Sorter(taskMasterList);
-    	sorter.sort();
-    	taskMasterList = sorter.getSortedList();
-    }
+	private void initFirstDisplay() {
+		//    	rootController.setFeedback("Welcome to fini");
+		//    	rootController.updateMainDisplay(taskMasterList);
+	}
+
+	public void executeCommand(String command) {
+		boolean isOperationSuccessful;
+		isOperationSuccessful = parser.parse(command);
+
+		// Change all of these hierarchy: TODO
+		rootController.updateMainDisplay(taskOrganiser.getTasks());
+		rootController.updateProjectsOverviewPanel(taskOrganiser.getTasks());
+		rootController.updateTasksOverviewPanel(taskOrganiser.getTasks());
+		rootController.updateDisplayToUser(isOperationSuccessful);
+		taskOrganiser.updateFile();
+
+		// Handle Using Command Class: TODO
+		// Command newCommand = new Command(command);
+		//
+		// Command.Type commandType = newCommand.getCommandType();
+		// String commandAction = newCommand.getCommandAction();
+		// String commandArguments = newCommand.getCommandArguments();
+	}
+
+	// Init Methods
+	public void setRootController(RootController rootController) {
+		this.rootController = rootController;
+	}
+
+	private void sortAllTasks() {
+		assert taskMasterList != null;
+		sorter = new Sorter(taskMasterList);
+		sorter.sort();
+		taskMasterList = sorter.getSortedList();
+	}
 }
