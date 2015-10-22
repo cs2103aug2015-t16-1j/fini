d6ac06cea5182d9f6c9cbb5cc066d11161ce2eac
Jonas <azeures@users.noreply.github.com>
2015-10-22 15:39:31 +0800

    Added getProjectName test unit for Project

---
+++ src/fini/main/tests/ProjectTest.java
@@ -0,0 +1,28 @@
+package fini.main.tests;
+
+import fini.main.model.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+
+
+public class ProjectTest {
+
+	Project proj;
+	
+	@Before
+	public void initialize() {
+		proj = new Project("testProject");
+	}
+	
+	
+	@Test
+	public void testGetProjectName() {
+		assertEquals("testProject", proj.getProjectName());
+	}
+
+}
