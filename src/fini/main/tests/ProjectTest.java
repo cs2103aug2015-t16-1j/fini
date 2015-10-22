1487d19f18ad8d66ec4016face5c189800a72987
Jonas <azeures@users.noreply.github.com>
2015-10-22 16:06:21 +0800

    Added removeTaskFromProject unit test for future testing

--- src/fini/main/tests/ProjectTest.java
+++ src/fini/main/tests/ProjectTest.java
@@ -8,11 +8,14 @@ import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 
+// @Author: Jonas
 
 public class ProjectTest {
 
 	Project proj;
 	Task testTask;
+	private static final String PASS = new String("PASS");
+	private static final String FAIL = new String("FAIL");
 	
 	@Before
 	public void initialize() {
@@ -20,6 +23,13 @@ public class ProjectTest {
 		testTask = new Task("unit test");
 	}
 	
+	
+	static public String test(int value) {
+		if (value == 1)
+			return PASS;
+		else
+			return FAIL;
+	}
 	
 	@Test
 	public void testGetProjectName() {
@@ -37,5 +47,16 @@ public class ProjectTest {
 		proj.addTaskToProject(testTask);
 		assertEquals("unit test", proj.getTasks().get(0).getTitle());
 	}
+	
+	/* Future Test
+	@Test
+	public void testRemoveTask() {
+		int result = 0;
+		proj.removeTaskFromProject(testTask);
+		if (proj.getTasks().isEmpty())
+			result = 1;
+		assertEquals("PASS", test(result));
+	}
+	*/
 
 }
