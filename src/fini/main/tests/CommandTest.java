939a63a70a98d9b037d2527e53c5d45eb8b929b0
Jonas <azeures@users.noreply.github.com>
2015-10-29 00:59:56 +0800

    Added unit test testAddCommand

--- src/fini/main/tests/CommandTest.java
+++ src/fini/main/tests/CommandTest.java
@@ -1,6 +1,8 @@
 package fini.main.tests;
 
 import fini.main.model.*;
+import fini.main.model.Command.CommandType;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
@@ -9,10 +11,35 @@ import org.junit.After;
 import org.junit.Test;
 
 public class CommandTest {
+	
+	private static final String PASS = new String("PASS");
+	private static final String FAIL = new String("FAIL");
 
+	Command addCommand;
+	Command deleteCommand;
+	Command updateCommand;
+	Command clearCommand;
+	int result;
+	
+	
+	@Before
+	public void initialize() {
+		addCommand = new Command("add task");
+		deleteCommand = new Command("delete task");
+		updateCommand = new Command("update 1");
+		clearCommand = new Command("clear");
+	}
+	
+	static public String test(int value) {
+		if (value == 1)
+			return PASS;
+		else
+			return FAIL;
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddCommand() {
+		assertEquals("ADD", addCommand.getCommandType().toString());
 	}
 
 }
