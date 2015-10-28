cb6215022b57b1e8f61e70db69adb44eba936a25
Jonas <azeures@users.noreply.github.com>
2015-10-29 00:47:55 +0800

    Added unit test for Command class

---
+++ src/fini/main/tests/CommandTest.java
@@ -0,0 +1,18 @@
+package fini.main.tests;
+
+import fini.main.model.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+public class CommandTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
