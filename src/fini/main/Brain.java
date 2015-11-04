e37154e9b4d757c11087169e00a1635be6d42749
Jonas <azeures@users.noreply.github.com>
2015-11-05 00:35:19 +0800

    Indentation for Brain

--- src/fini/main/Brain.java
+++ src/fini/main/Brain.java
@@ -31,10 +31,10 @@ public class Brain {
 	private StatusSaver statusSaver;
 
 	private ArrayList<Task> taskMasterList;
-	private ObservableList<String> projectNameList = FXCollections.observableArrayList();
+	private ObservableList<String> projectNameList = FXCollections.observableArrayList(); 
 	private ObservableList<Task> taskObservableList = FXCollections.observableArrayList();
 	private ObservableList<Task> taskAuxiliaryList = FXCollections.observableArrayList(); 
-	
+
 	private boolean searchDisplayTrigger = false;
 	private boolean projectDisplayTrigger = false;
 	private boolean completeDisplayTrigger = false;
@@ -44,14 +44,14 @@ public class Brain {
 		finiParser = FiniParser.getInstance();
 		taskOrganiser = Storage.getInstance();
 		statusSaver = StatusSaver.getInstance();
-		
+
 		// Everything stored here in Brain unless an updateFile is executed
 		// taskMasterList: all existing tasks
 		// taskObservableList: all displayed tasks
 		taskMasterList = taskOrganiser.readFile();
 		sortTaskMasterList();
 		taskObservableList.setAll(taskMasterList.stream().filter(task -> !task.isCompleted()).collect(Collectors.toList()));
-		taskAuxiliaryList.setAll(taskObservableList);
+		taskAuxiliaryList.setAll(taskObservableList); 
 		for (Task task : taskAuxiliaryList) {
 			if (!projectNameList.contains(task.getProjectName())) {
 				projectNameList.add(task.getProjectName());
@@ -71,7 +71,7 @@ public class Brain {
 	public void initDisplay() {
 		rootController.setFocusToCommandBox();
 		rootController.updateMainDisplay(taskAuxiliaryList);
-		rootController.updateProjectsOverviewPanel(taskAuxiliaryList);
+		rootController.updateProjectsOverviewPanel(projectNameList);
 		rootController.updateTasksOverviewPanel(taskAuxiliaryList);
 		rootController.updateFiniPoints(taskMasterList.stream().filter(task -> task.isCompleted()).collect(Collectors.toList()));
 	}
@@ -81,7 +81,7 @@ public class Brain {
 		CommandType commandType = newCommand.getCommandType();
 		int objectIndex = newCommand.getObjectIndex();
 		String commandParameters = newCommand.getCommandParameters();
-		
+
 		String display = "";
 		switch (commandType) {
 		case ADD:
@@ -113,9 +113,9 @@ public class Brain {
 			display = displayTask(commandParameters);
 			break;
 		case SEARCH:
-		    display = "Searching...";
-		    searchTask(commandParameters);
-		    break;
+			display = "Searching...";
+			searchTask(commandParameters);
+			break;
 		case MODS:
 			display = loadNUSMods(commandParameters);
 			saveThisStatus();
@@ -130,10 +130,6 @@ public class Brain {
 			display = uncompleteTask(objectIndex);
 			saveThisStatus();
 			break;
-		case HELP:
-			display = displayHelp();
-			saveThisStatus();
-			break;
 		case INVALID:
 			display = "Invalid command. Please type help for assistance.";
 			break;
@@ -142,27 +138,20 @@ public class Brain {
 		}
 
 		displayControl();
-		
+
 		sortTaskMasterList();
 		taskAuxiliaryList.setAll(taskMasterList.stream().filter(task -> !task.isCompleted()).collect(Collectors.toList()));
-		
 		projectNameList = FXCollections.observableArrayList();
 		for (Task task : taskAuxiliaryList) {
 			if (!projectNameList.contains(task.getProjectName())) {
 				projectNameList.add(task.getProjectName());
 			}
 		}
-		
-		rootController.updateProjectsOverviewPanel(taskAuxiliaryList);
+		rootController.updateProjectsOverviewPanel(projectNameList);
 		rootController.updateTasksOverviewPanel(taskAuxiliaryList);
-		
+
 		rootController.updateDisplayToUser(display);
 		rootController.updateFiniPoints(taskMasterList.stream().filter(task -> task.isCompleted()).collect(Collectors.toList()));
-	}
-	
-	private String displayHelp() {
-		rootController.displayHelpPanel();
-		return "Check the help panel";
 	}
 
 	// Display Control Methods
@@ -171,7 +160,7 @@ public class Brain {
 			taskObservableList.setAll(taskMasterList.stream().filter(task -> task.isCompleted()).collect(Collectors.toList()));
 			rootController.updateCompletedDisplay(taskObservableList);
 		} else if (searchDisplayTrigger) {
-		    rootController.updateSearchDisplay(taskObservableList);
+			rootController.updateSearchDisplay(taskObservableList);
 		} else if (allDisplayTrigger) {
 			rootController.updateAllDisplay(taskObservableList);
 		} else if (projectDisplayTrigger) {
@@ -179,96 +168,96 @@ public class Brain {
 		} else {
 			sortTaskMasterList();
 			taskObservableList.setAll(taskMasterList.stream().filter(task -> !task.isCompleted()).collect(Collectors.toList()));
-//			for (Task task : taskObservableList) {
-//				System.out.println(task);
-//			}
+			//			for (Task task : taskObservableList) {
+			//				System.out.println(task);
+			//			}
 			rootController.updateMainDisplay(taskObservableList);
 		}
 	}
-	
+
 	// Logic Methods
 	private String addTask(String commandParameters) {
 		if (commandParameters.isEmpty()) {
 			return "CommandParameters is empty";
 		}
-		
+
 		finiParser.parse(commandParameters);
-		
+
 		if (finiParser.getDatetimes() != null && finiParser.getDatetimes().size() == 2 &&
 				!finiParser.getDatetimes().get(0).isBefore(finiParser.getDatetimes().get(1))) {
 			return "Start date and time should be earlier than end date time";
 		}
-		
+
 		if (!finiParser.getProjectName().equals("Inbox") &&
 				!projectNameList.contains(finiParser.getProjectName()) &&
 				projectNameList.size() == 5) {
-				return "Maximum 5 projects at the same time";
-			}
-		
+			return "Maximum 5 projects at the same time";
+		}
+
 		Task newTask = new Task.TaskBuilder(finiParser.getNotParsed(), finiParser.getIsRecurring())
-						   .setDatetimes(finiParser.getDatetimes())
-						   .setPriority(finiParser.getPriority())
-						   .setProjectName(finiParser.getProjectName())
-						   .setInterval(finiParser.getInterval())
-						   .setRecursUntil(finiParser.getRecursUntil()).build();
-		
+				.setDatetimes(finiParser.getDatetimes())
+				.setPriority(finiParser.getPriority())
+				.setProjectName(finiParser.getProjectName())
+				.setInterval(finiParser.getInterval())
+				.setRecursUntil(finiParser.getRecursUntil()).build();
+
 		taskMasterList.add(newTask);
 		taskOrganiser.updateFile(taskMasterList);
 		return "Added: " + finiParser.getNotParsed();
 	}
-	
+
 	private String updateTask(int objectIndex, String commandParameters) {
 		Task taskToUpdate;
-		
+
 		if (commandParameters.isEmpty()) {
 			return "CommandParameters is empty";
 		}
-		
+
 		try {
 			taskToUpdate = taskObservableList.get(objectIndex - 1);
 		} catch (IndexOutOfBoundsException e) {
 			return "Task not found";
 		}
-		
+
 		// delete first
 		taskObservableList.remove(taskToUpdate);
 		taskMasterList.remove(taskToUpdate);
 		taskOrganiser.updateFile(taskMasterList);
-		
+
 		// add then
 		finiParser.parse(commandParameters);
-		
+
 		if (finiParser.getDatetimes() != null && finiParser.getDatetimes().size() == 2 &&
 				!finiParser.getDatetimes().get(0).isBefore(finiParser.getDatetimes().get(1))) {
 			return "Start date and time should be earlier than end date time";
 		}
-		
+
 		if (!finiParser.getProjectName().equals("Inbox") &&
 				!projectNameList.contains(finiParser.getProjectName()) &&
 				projectNameList.size() == 5) {
-				return "Maximum 5 projects at the same time";
-			}
-		
+			return "Maximum 5 projects at the same time";
+		}
+
 		Task newTask = new Task.TaskBuilder(finiParser.getNotParsed(), finiParser.getIsRecurring())
-						   .setDatetimes(finiParser.getDatetimes())
-						   .setPriority(finiParser.getPriority())
-						   .setProjectName(finiParser.getProjectName())
-						   .setInterval(finiParser.getInterval())
-						   .setRecursUntil(finiParser.getRecursUntil()).build();
-		
+				.setDatetimes(finiParser.getDatetimes())
+				.setPriority(finiParser.getPriority())
+				.setProjectName(finiParser.getProjectName())
+				.setInterval(finiParser.getInterval())
+				.setRecursUntil(finiParser.getRecursUntil()).build();
+
 		taskMasterList.add(newTask);
 		taskOrganiser.updateFile(taskMasterList);
-		
+
 		return "Update: " + objectIndex + taskToUpdate.getTitle();
 	}
-	
+
 	// @author A0121828H
 	private String clearAllTasks() {
 		taskMasterList.clear();
 		taskOrganiser.updateFile(taskMasterList);
 		return "All tasks have been cleared";
 	}
-	
+
 	private String deleteTask(int objectIndex) {
 		Task taskToDelete;
 		try {
@@ -276,13 +265,13 @@ public class Brain {
 		} catch (IndexOutOfBoundsException e) {
 			return "Task not found";
 		}
-		
+
 		taskObservableList.remove(taskToDelete);
 		taskMasterList.remove(taskToDelete);
 		taskOrganiser.updateFile(taskMasterList);
 		return "Delete: " + objectIndex + " " + taskToDelete.getTitle();
 	}
-	
+
 	private String completeTask(int objectIndex) {
 		Task taskToComplete;
 		try {
@@ -290,12 +279,12 @@ public class Brain {
 		} catch (IndexOutOfBoundsException e) {
 			return "Task not found";
 		}
-		
+
 		if (taskToComplete.isRecurring() && taskToComplete.hasNext()) {
 			Task copyTask = taskToComplete.makeCopy();
 			copyTask.setIsComplete();
 			copyTask.updateObjectID();
-			
+
 			for (Iterator<Task> iterator = taskMasterList.iterator(); iterator.hasNext(); ) {
 				Task taskToRemove = iterator.next();
 				if (!taskToRemove.getObjectID().equals(taskToComplete.getObjectID()) &&
@@ -312,7 +301,7 @@ public class Brain {
 		taskOrganiser.updateFile(taskMasterList);
 		return "Complete: " + objectIndex + taskToComplete.getTitle();
 	}
-	
+
 	private String uncompleteTask(int objectIndex) {
 		Task taskToUncomplete;
 		try {
@@ -350,7 +339,7 @@ public class Brain {
 		taskOrganiser.updateFile(taskMasterList);
 		return "NUSMODS loaded";
 	}
-	
+
 	private String undo() {
 		if (statusSaver.isUndoMasterStackEmpty()) {
 			return "Cannot undo lah! You haven't done any changes yet.";
@@ -361,7 +350,7 @@ public class Brain {
 		taskOrganiser.updateFile(taskMasterList);
 		return "Undo~do~do~do~do~";
 	}
-	
+
 	private String redo() {
 		if (statusSaver.isRedoMasterStackEmpty()) {
 			return "Cannot redo lah! You dun have anything to redo alrdy.";
@@ -372,25 +361,28 @@ public class Brain {
 		taskOrganiser.updateFile(taskMasterList);
 		return "Redo~do~do~do~do~";
 	}
-	
+
 	private String setUserPrefDirectory(String commandParameters) {
 		return taskOrganiser.setUserPrefDirectory(commandParameters);
 	}
-	
+
 	private String displayTask(String commandParameters) {
 		if (commandParameters.equals("completed")) {
 			completeDisplayTrigger = true;
+			projectDisplayTrigger = false;
 			searchDisplayTrigger = false;
 			allDisplayTrigger = false;
 			return "display completed";
 		} else if(commandParameters.equals("") || commandParameters.equals("main")) {
 			completeDisplayTrigger = false;
+			projectDisplayTrigger = false;
 			searchDisplayTrigger = false;
 			allDisplayTrigger = false;
 			return "display main";
 		} else if(commandParameters.equals("all")) {
 			completeDisplayTrigger = false;
 			searchDisplayTrigger = false;
+			projectDisplayTrigger = false;
 			allDisplayTrigger = true;
 			sortTaskMasterListWithIncomplete();
 			taskObservableList.setAll(taskMasterList);
@@ -412,31 +404,31 @@ public class Brain {
 			return "displayTask method";
 		}
 	}
-	
+
 	private void searchTask(String commandParameters) {
-        searchDisplayTrigger = true;
-        ObservableList<Task> tempObservableList = FXCollections.observableArrayList();
-        finiParser.parse(commandParameters);
-        ArrayList<LocalDateTime> searchDateTimes = finiParser.getDatetimes();
-        for (Task task : taskMasterList) {
-            if (task.getTitle().contains(commandParameters)) {
-                tempObservableList.add(task);
-            }
-        }
-        taskObservableList.setAll(tempObservableList);
-    }
-	
+		searchDisplayTrigger = true;
+		ObservableList<Task> tempObservableList = FXCollections.observableArrayList();
+		finiParser.parse(commandParameters);
+		ArrayList<LocalDateTime> searchDateTimes = finiParser.getDatetimes();
+		for (Task task : taskMasterList) {
+			if (task.getTitle().contains(commandParameters)) {
+				tempObservableList.add(task);
+			}
+		}
+		taskObservableList.setAll(tempObservableList);
+	}
+
 	// Utilization Methods
 	private void saveThisStatus() {
 		assert taskMasterList != null;
 		assert taskObservableList != null;
 		statusSaver.saveStatus(taskMasterList, taskObservableList);
 	}
-	
+
 	public ObservableList<Task> getTaskObservableList() {
 		return taskObservableList;
 	}
-	
+
 	// Initialization Methods
 	public void setRootController(RootController rootController) {
 		this.rootController = rootController;
@@ -448,7 +440,7 @@ public class Brain {
 		sorter.sort();
 		taskMasterList = sorter.getSortedList();
 	}
-	
+
 	private void sortTaskMasterListWithIncomplete() {
 		assert taskMasterList != null;
 		sorter = new Sorter(taskMasterList);
