7df9aed8c541427549cd0604f2360a7f16fb7489
gaieepo <gaieepo@gmail.com>
2015-11-05 02:37:30 +0800

    storage test for constructor, read and update

--- src/fini/main/tests/StorageTest.java
+++ src/fini/main/tests/StorageTest.java
@@ -22,29 +22,29 @@ public class StorageTest {
 	File saveFile = new File("save.txt");
 	File configFile = new File("config.txt");
 	File userPrefFile = new File("Fini_untitled.txt");
-	
+
 	PrintWriter writer;
 	BufferedReader reader;
-	
+
 	@Test
 	public void testSettings() {
 		saveFile.delete();
 		configFile.delete();
 		userPrefFile.delete();
-		
+
 		Storage testStorage = Storage.getInstance();
-		
+
 		assertEquals(true, saveFile.exists());
 		assertEquals(true, configFile.exists());
 		assertEquals(true, userPrefFile.exists());
 	}
-	
+
 	@Test
 	public void testReadAndUpdateFile() {
 		Storage testStorage = Storage.getInstance();
 		String[] data = {"curry chicken tomorrow morning project gai", 
-						 "gaieepo the day before the third sat of dec", 
-						 "harish tomorrow 5am repeat every two days priority high"};
+				"gaieepo the day before the third sat of dec", 
+		"harish tomorrow 5am repeat every two days priority high"};
 		ArrayList<Task> inputData = createTasks(data);
 		assertEquals(true, testStorage.updateFile(inputData));
 		ArrayList<Task> outputData = testStorage.readFile();
@@ -58,27 +58,27 @@ public class StorageTest {
 			assertEquals(inputData.get(i).getProjectName(), inputData.get(i).getProjectName());
 		}
 	}
-	
-//	assertEquals("No such file", testStorage.setUserPrefDirectory("/home/gaieepo/Videos/test.txt"));
-//	assertEquals("Invalid path", testStorage.setUserPrefDirectory("/home/g"));
-//	assertEquals("Invalid path", testStorage.setUserPrefDirectory("/home/gaieepo/Videos/tes?t.txt"));
-//	assertEquals("Invalid path", testStorage.setUserPrefDirectory("/home/gaieepo/Videos/"));
-	
+
+	//	assertEquals("No such file", testStorage.setUserPrefDirectory("/home/gaieepo/Videos/test.txt"));
+	//	assertEquals("Invalid path", testStorage.setUserPrefDirectory("/home/g"));
+	//	assertEquals("Invalid path", testStorage.setUserPrefDirectory("/home/gaieepo/Videos/tes?t.txt"));
+	//	assertEquals("Invalid path", testStorage.setUserPrefDirectory("/home/gaieepo/Videos/"));
+
 	private ArrayList<Task> createTasks(String[] data) {
 		FiniParser finiParser = FiniParser.getInstance();
 		ArrayList<Task> tasks = new ArrayList<Task>();
 		for (String taskString : data) {
 			finiParser.parse(taskString);
 			tasks.add(new Task.TaskBuilder(finiParser.getNotParsed(), finiParser.getIsRecurring())
-						.setDatetimes(finiParser.getDatetimes())
-						.setPriority(finiParser.getPriority())
-						.setProjectName(finiParser.getProjectName())
-						.setInterval(finiParser.getInterval())
-						.setRecursUntil(finiParser.getRecursUntil()).build());
+					.setDatetimes(finiParser.getDatetimes())
+					.setPriority(finiParser.getPriority())
+					.setProjectName(finiParser.getProjectName())
+					.setInterval(finiParser.getInterval())
+					.setRecursUntil(finiParser.getRecursUntil()).build());
 		}
 		return tasks;
 	}
-	
+
 	@After
 	public void cleanUp() {
 		saveFile.delete();
