f826c0457df2939fca7a38374218857d3e2bde52
gaieepo <gaieepo@gmail.com>
2015-11-07 20:37:10 +0800

    fix indentations

--- src/fini/main/model/Command.java
+++ src/fini/main/model/Command.java
@@ -3,80 +3,80 @@ package fini.main.model;
 import java.util.Arrays;
 
 public class Command {
-	public static enum CommandType {
-		ADD, UPDATE, DELETE, CLEAR, HELP, SET, REDO, DISPLAY, SEARCH, UNDO, EXIT, COMPLETE, UNCOMPLETE, MODS, INVALID
-	};
-	
-	private CommandType commandType;
-	private String userCommand;
-	private int objectIndex = -1;
+    public static enum CommandType {
+        ADD, UPDATE, DELETE, CLEAR, HELP, SET, REDO, DISPLAY, SEARCH, UNDO, EXIT, COMPLETE, UNCOMPLETE, MODS, INVALID
+    };
 
-	private String commandParameters = null;
-	
-	public Command(String userInput) {
-		String[] splitUserInput = userInput.trim().split("\\s+");
-		userCommand = splitUserInput[0].toLowerCase();
-		commandType = determineCommandType(userCommand);
-		
-		if (commandType == CommandType.UPDATE ||
-			commandType == CommandType.DELETE ||
-			commandType == CommandType.COMPLETE ||
-			commandType == CommandType.UNCOMPLETE) {
-			try {
-				objectIndex = Integer.parseInt(splitUserInput[1]);
-			} catch (IndexOutOfBoundsException | NumberFormatException e) {
-				commandType = CommandType.INVALID;
-			}
-			commandParameters = String.join(" ", Arrays.copyOfRange(splitUserInput, 2, splitUserInput.length));
-		} else {
-			commandParameters = String.join(" ", Arrays.copyOfRange(splitUserInput, 1, splitUserInput.length));
-		}
-	}
-	
-	public CommandType getCommandType() {
-		return commandType;
-	}
-	
-	public int getObjectIndex() {
-		return objectIndex;
-	}
-	
-	public String getCommandParameters() {
-		return commandParameters;
-	}
-	
-	private CommandType determineCommandType(String userCommand) {
-		switch (userCommand) {
-		case "add":
-			return CommandType.ADD;
-		case "update":
-			return CommandType.UPDATE;
-		case "delete":
-			return CommandType.DELETE;
-		case "clear":
-			return CommandType.CLEAR;
-		case "undo":
-			return CommandType.UNDO;
-		case "redo":
-			return CommandType.REDO;
-		case "set":
-			return CommandType.SET;
-		case "display":
-			return CommandType.DISPLAY;
-		case "search":
-		    return CommandType.SEARCH;
-		case "exit":
-			return CommandType.EXIT;
-		case "complete":
-			return CommandType.COMPLETE;
-		case "uncomplete":
-			return CommandType.UNCOMPLETE;
-		case "mods":
-			return CommandType.MODS;
-		case "help":
-			return CommandType.HELP;
-		default:
-			return CommandType.INVALID;
-		}
-	}
+    private CommandType commandType;
+    private String userCommand;
+    private int objectIndex = -1;
+
+    private String commandParameters = null;
+
+    public Command(String userInput) {
+        String[] splitUserInput = userInput.trim().split("\\s+");
+        userCommand = splitUserInput[0].toLowerCase();
+        commandType = determineCommandType(userCommand);
+
+        if (commandType == CommandType.UPDATE ||
+                commandType == CommandType.DELETE ||
+                commandType == CommandType.COMPLETE ||
+                commandType == CommandType.UNCOMPLETE) {
+            try {
+                objectIndex = Integer.parseInt(splitUserInput[1]);
+            } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                commandType = CommandType.INVALID;
+            }
+            commandParameters = String.join(" ", Arrays.copyOfRange(splitUserInput, 2, splitUserInput.length));
+        } else {
+            commandParameters = String.join(" ", Arrays.copyOfRange(splitUserInput, 1, splitUserInput.length));
+        }
+    }
+
+    public CommandType getCommandType() {
+        return commandType;
+    }
+
+    public int getObjectIndex() {
+        return objectIndex;
+    }
+
+    public String getCommandParameters() {
+        return commandParameters;
+    }
+
+    private CommandType determineCommandType(String userCommand) {
+        switch (userCommand) {
+            case "add":
+                return CommandType.ADD;
+            case "update":
+                return CommandType.UPDATE;
+            case "delete":
+                return CommandType.DELETE;
+            case "clear":
+                return CommandType.CLEAR;
+            case "undo":
+                return CommandType.UNDO;
+            case "redo":
+                return CommandType.REDO;
+            case "set":
+                return CommandType.SET;
+            case "display":
+                return CommandType.DISPLAY;
+            case "search":
+                return CommandType.SEARCH;
+            case "exit":
+                return CommandType.EXIT;
+            case "complete":
+                return CommandType.COMPLETE;
+            case "uncomplete":
+                return CommandType.UNCOMPLETE;
+            case "mods":
+                return CommandType.MODS;
+            case "help":
+                return CommandType.HELP;
+            default:
+                return CommandType.INVALID;
+        }
+    }
 }
--- src/fini/main/model/Storage.java
+++ src/fini/main/model/Storage.java
@@ -18,155 +18,155 @@ import com.google.gson.JsonSyntaxException;
  *
  */
 public class Storage {
-	private static Storage taskOrganiser;
+    private static Storage taskOrganiser;
 
-	private File saveFile;
-	private File configFile;
-	private File userPrefFile;
-	
-	private String userPrefFileName;
-	
-	private BufferedReader reader;
-	private PrintWriter writer;
+    private File saveFile;
+    private File configFile;
+    private File userPrefFile;
 
-	private Gson gson;
+    private String userPrefFileName;
 
-	public static Storage getInstance() {
-		if (taskOrganiser == null) {
-			taskOrganiser = new Storage();
-		}
-		return taskOrganiser;
-	}
+    private BufferedReader reader;
+    private PrintWriter writer;
 
-	public Storage() {
-		gson = new Gson();
+    private Gson gson;
 
-		saveFile = new File("save.txt");
-		createIfNotExists(saveFile);
-		
-		configFile = new File("config.txt");
-		createIfNotExists(configFile);
-		
-		userPrefFileName = getUserPrefFileName(configFile);
-		updateConfigFile(userPrefFileName);
-		userPrefFile = new File(userPrefFileName);
-		createIfNotExists(userPrefFile);
-	}
+    public static Storage getInstance() {
+        if (taskOrganiser == null) {
+            taskOrganiser = new Storage();
+        }
+        return taskOrganiser;
+    }
 
-	public ArrayList<Task> readFile() {
-		ArrayList<Task> tasks = new ArrayList<Task>();
-		tasks = readTasks(userPrefFile);
-		if (tasks == null || tasks.isEmpty()) {
-			tasks = readTasks(saveFile);
-			if (tasks == null || tasks.isEmpty()) {
-				tasks = new ArrayList<Task>();
-			}
-		}
-		return tasks;
-	}
-	
-	private ArrayList<Task> readTasks(File file) {
-		String text = "";
-		ArrayList<Task> tasks = new ArrayList<Task>();
-		try {
-			if (!initReader(file)) {
-				return tasks;
-			}
-			while ((text = reader.readLine()) != null) {
-				Task task = gson.fromJson(text, Task.class);
-				tasks.add(task);
-			}
-		} catch (IOException | JsonSyntaxException e) {
-			e.printStackTrace();
-			return null;
-		}
-		closeReader();
-		return tasks;
-	}
+    public Storage() {
+        gson = new Gson();
 
-	public boolean updateFile(ArrayList<Task> tasks) {
-		return updateTasks(saveFile, tasks) && updateTasks(userPrefFile, tasks);
-	}
-	
-	private boolean updateTasks(File file, ArrayList<Task> tasks) {
-		try {
-			writer = new PrintWriter(file, "UTF-8");
-			for (Task task : tasks) {
-				writer.println(gson.toJson(task));
-			}
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return false;
-		}
-		writer.close();
-		return true;
-	}
-	
-	public String setUserPrefDirectory(String filePath) {
-		userPrefFileName = filePath;
-		File userFile = new File(userPrefFileName);
-		
-		if (userFile.equals(userPrefFile)) {
-			return "Same file directory";
-		} else if (userFile.exists()) {
-			updateConfigFile(userPrefFileName);
-			userPrefFile = userFile;
-			return "The directory is set";
-		} else {
-			return "No such file";
-		}
-	}
-	
-	private void updateConfigFile(String fileName) {
-		try {
-			writer = new PrintWriter(configFile, "UTF-8");
-			writer.println(fileName);
-			writer.close();
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
+        saveFile = new File("save.txt");
+        createIfNotExists(saveFile);
 
-	// Initialization Methods
-	private boolean initReader(File saveFile) {
-		try {
-			reader = new BufferedReader(new FileReader(saveFile));
-		} catch (FileNotFoundException e) {
-			return false;
-		}
-		return true;
-	}
+        configFile = new File("config.txt");
+        createIfNotExists(configFile);
 
-	private void closeReader() {
-		try {
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        userPrefFileName = getUserPrefFileName(configFile);
+        updateConfigFile(userPrefFileName);
+        userPrefFile = new File(userPrefFileName);
+        createIfNotExists(userPrefFile);
+    }
 
-	// Utility Methods
-	private void createIfNotExists(File file) {
-		try {
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private String getUserPrefFileName(File configFile) {
-		initReader(configFile);
-		String fileName = "";
-		try {
-			if ((fileName = reader.readLine()) == null) {
-				fileName = "Fini_untitled.txt";
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		closeReader();
-		return fileName;
-	}
+    public ArrayList<Task> readFile() {
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        tasks = readTasks(userPrefFile);
+        if (tasks == null || tasks.isEmpty()) {
+            tasks = readTasks(saveFile);
+            if (tasks == null || tasks.isEmpty()) {
+                tasks = new ArrayList<Task>();
+            }
+        }
+        return tasks;
+    }
+
+    private ArrayList<Task> readTasks(File file) {
+        String text = "";
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        try {
+            if (!initReader(file)) {
+                return tasks;
+            }
+            while ((text = reader.readLine()) != null) {
+                Task task = gson.fromJson(text, Task.class);
+                tasks.add(task);
+            }
+        } catch (IOException | JsonSyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
+        closeReader();
+        return tasks;
+    }
+
+    public boolean updateFile(ArrayList<Task> tasks) {
+        return updateTasks(saveFile, tasks) && updateTasks(userPrefFile, tasks);
+    }
+
+    private boolean updateTasks(File file, ArrayList<Task> tasks) {
+        try {
+            writer = new PrintWriter(file, "UTF-8");
+            for (Task task : tasks) {
+                writer.println(gson.toJson(task));
+            }
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return false;
+        }
+        writer.close();
+        return true;
+    }
+
+    public String setUserPrefDirectory(String filePath) {
+        userPrefFileName = filePath;
+        File userFile = new File(userPrefFileName);
+
+        if (userFile.equals(userPrefFile)) {
+            return "Same file directory";
+        } else if (userFile.exists()) {
+            updateConfigFile(userPrefFileName);
+            userPrefFile = userFile;
+            return "The directory is set";
+        } else {
+            return "No such file";
+        }
+    }
+
+    private void updateConfigFile(String fileName) {
+        try {
+            writer = new PrintWriter(configFile, "UTF-8");
+            writer.println(fileName);
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Initialization Methods
+    private boolean initReader(File saveFile) {
+        try {
+            reader = new BufferedReader(new FileReader(saveFile));
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void closeReader() {
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Utility Methods
+    private void createIfNotExists(File file) {
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getUserPrefFileName(File configFile) {
+        initReader(configFile);
+        String fileName = "";
+        try {
+            if ((fileName = reader.readLine()) == null) {
+                fileName = "Fini_untitled.txt";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        closeReader();
+        return fileName;
+    }
 }
